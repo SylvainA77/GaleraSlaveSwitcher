ac85cd253bcf0d3eba161a2954a34babfaa786e5
Sylvain Arbaudie <arbaudie.it@gmail.com>
2019-07-10 21:16:26 +0200

    initial commit
    switchover is intended to be a function lib that can be sourced in different bash scripts. 
    
    it provides 3 base functions : 1. sqlexec intended to execute sql statement and fetch resultats in a shell friendly way , 2. maxexec is intended to execute commands to maxscale and fetch results in a shell friendly way , 3. switchover is intended to execute all the necessary steps to switch one slavetoits new master.

---
+++ switchover.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# switchover.sh
+# just a function lib
+# needs : 1. slave ip,
+#         2. new master ip,
+#         3. user with replication client on both slave & whole cluster : maxscale monitor user is a good fit
+  exec 1>/var/log/switchover.log
+  exec 2>/var/log/switchover.err
+
+sqlexec()
+{
+        [ $# -ne 2 ] && echo "sqlexec function requires 2 args : 1. credentials, 2. sql statement to execute" && exit -2
+        mysql -B --skip_column_names $1 -e $2
+        exit $?
+}
+
+maxexec()
+{
+        [ $# -ne 1 ] && echo "maxexec function requires only 1 args : 1. maxscale request to execute" && exit -3
+        maxctrl --tsv $1
+        exit $?
+}
+
+switchover()
+        [ $# -ne 4 ] && echo "Switchover fucnction requires 4 args : 1. slave ip, 2. new master ip, 3. user, 4. password" && exit -1
+
+        mastercredentials="-u$3 -p$4 -h $2"
+        slavecredentials="-u$3 -p$4 -h $1"
+
+        #1 wait until slave is fully up to date
+        while [ $readlogpos -ne $execlogpos ]
+        do
+                read readlogpos execlogpos <<<$( sqlexec $slavecredentials 'show slave status' | cut -f7,22 )
+        done
+
+        #2   find the watermark on the slave
+        #2.1 find the last binlog file
+        slavelastbinlogfile=$( sqlexec $slavecredentials 'show master status' | cut -f1 )
+
+        #2.2 find the watermark (xid) on the last binlog entry
+        watermark=$( sqlexec $slavecredentials "show binlog events in '$binlogfile'" | grep -i commit | tail -1 | cut -f6 | cut -d'*' -f2 | xargs )
+
+        #3   find the watermark on the new master
+        #3.1 find the list of binlog files in reverse order
+        newmasterbinlogfiles =( $( sqlexec $mastercredentials 'show binary logs' | cut -f1 | tac ) )
+
+        for eachbinlogfile in "${newmasterbinlogfiles[@]}"
+        do
+                watermarkGTID=$( sqlexec $mastercredentials "show binlog events in '$eachbinlogfile'" | grep -i -e commit -e begin | grep -i -e "$watermark" -B1 | head -1 | cut -f6 | cut -d' ' -f3)
+                [[ ! -z "$watermarkGTID" ]] && break # as long as watermark is not matched, waterarkGTID stays unset/empty. Once watermarkGTID is set, we have found what we need and can exit the loop
+        done
+
+        #4 change slave settings and reconnect
+        #4.1 stop slave
+        sqlexec $slavecredentials 'stop slave'
+
+        #4.2 change slave gtid
+        sqlexec $slavecredentials "set global gtid_slave_pos=$watermakrGTID"
+        
+        #4.3 change master shot
+        sqlexec $slavecredentials "change master to master_host=$newmasterip, master_use_gtid=slave_pos"
+
+        #4.4 start slave
+        sqlexec $slavecredentials 'start slave'
+
+return $retcode
+}
+
+exit 0
