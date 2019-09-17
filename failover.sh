ae4bb20d41e3c2b2f8bb2f8034ab16bbdb41eef3
Sylvain Arbaudie <arbaudie.it@gmail.com>
2019-09-18 00:51:25 +0200

    Update failover.sh
    added --target parameter
    
    disabled maxscale lookup when new master ip is provided

--- failover.sh
+++ failover.sh
@@ -13,22 +13,33 @@
 # github repo : https://github.com/SylvainA77/GaleraSlaveSwitcher
 ###
 # this bash script is intended to be triggered by maxscale event API
-# hence only 2 args are exepected : failed master ip --initiator=$INITIATOR
-# and list of slaves to switchover --children=$CHILDREN
-#TODO : add optional --target=$NEWMASTERIP parameter for manual triggering w/out maxscale
+# up to 4 args are expected : failed master ip --initiator=$INITIATOR
+#                               format of $initiator is : [IP]:port
+#                             list of slaves to switchover --children=$CHILDREN
+#                               format of $children is : [IP]:port,[IP]:port,*
+#                             name of the monitor which detect the master_down event : --monitor=monitorname
+#                             (optional) target new master : --target=[IP]:port
+#                               target disable new master lookup in maxscale
+###
+# alternatively can be called from command line with only 2 args : 
+#                           target new master : --target=[IP]:port
+#                           list of slaves to switch over : --children=[IP]:port,[IP]:port,*
+###
+#TODO
 ###
 # stderr & logs are sent to /var/log/failover.err
 
+set +x
 
 exec 2>/var/log/failover.err
 
 debug=1
 
-# we need all those juicy functions don't we ?
+# we need all those juicy functions, don't we ?
 source /var/lib/lib-switchover.sh
 source /etc/.credentials
 
-ARGS=$(getopt -o '' --long 'initiator:,children:,monitor:' -- "$@")
+ARGS=$(getopt -o '' --long 'initiator:,children:,monitor:,target:' -- "$@")
 
 eval set -- "$ARGS"
 
@@ -47,6 +58,11 @@ while true; do
         --monitor)
             shift;
             monitor=$1
+            shift;
+        ;;
+        --target)
+            shift;
+            target=$1
             shift;
         ;;
         --)
@@ -70,12 +86,18 @@ do
 done
 
 #2   find the new master
-failedmaster=$( echo $initiator | cut -d'[' -f2 | cut -d']' -f1 )
-masterip=$( findnewmaster $failedmaster $monitor )
-[[ -n "$debug" ]] && echoerr "newmasterip:$masterip"
+#2.1 if --target, then bypass maxscale
+[[ -n "$target" ]] && masterip=$( echo $target | cut -d'[' -f2 | cut -d']' -f1 )
+[[ -n "$debug" ]] && echoerr "target:$target"
 
-#3   perform the switchover on every oprhaned child
+#2.2 if --initiator, then call maxscale
+[[ -n $"initiator" ]] && failedmaster=$( echo $initiator | cut -d'[' -f2 | cut -d']' -f1 )
+[[ -n $"initiator" ]] && masterip=$( findnewmaster $failedmaster $monitor )
+[[ -n "$debug" ]] && echoerr "initiator:$initiator"
 
+[[ -n "$debug" ]] && echoerr "masterip:$masterip"
+
+#3   perform the switchover on every oprhaned child to $masterip
 # format of $children is : [IP]:port,[IP]:port,*
 # so we have to break the string into an array of strings using , as a separator
 IFS=',' read -ra childrens <<< "$children"
